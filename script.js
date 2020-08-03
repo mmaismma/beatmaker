46c1e1d9a884da34935445848a1157f5f67b0c35
mmaismma <mmaismma@hotmail.com>
2020-08-03 13:27:37 +0530

    🆕Added all beats

---
+++ script.js
@@ -0,0 +1,12 @@
+const tileArray = document.getElementsByClassName('tile');
+for (i = 0; i < tileArray.length; i++) {
+    tileArray[i].addEventListener('click', play);
+}
+let a;
+function play(event) {
+    //let audioNumber = event.target;
+    console.log('mmma');
+    //console.log(audioNumber);
+    //console.log(tileArray[x]);
+    //console.log(x);
+}
