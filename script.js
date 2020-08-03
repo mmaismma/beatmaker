ba76634501ec7e0aa5269a392303a2dd5631806e
mmaismma <mmaismma@hotmail.com>
2020-08-03 13:42:24 +0530

    ✔Playing is all working

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const tileArray = document.getElementsByClassName('tile');
 for (i = 0; i < tileArray.length; i++) {
-    tileArray[i].addEventListener('click', play);
+    let idNo = tileArray[i].classList[2];
+    tileArray[i].addEventListener('click', () => { play(idNo) });
 }
 let a;
-function play(event) {
-    //let audioNumber = event.target;
-    console.log('mmma');
+function play(x) {
+    console.log(x);
+    let audio = new Audio(`Resources/Beats/0_${x}.flac`)
+    audio.play();
     //console.log(audioNumber);
     //console.log(tileArray[x]);
     //console.log(x);
