7c6fee2ef0f14ad68d6bfd51d5bbcf213131f1d6
mmaismma <mmaismma@hotmail.com>
2020-08-04 13:46:37 +0530

    ✔Fundamental principles are done

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const tileArray = document.getElementsByClassName('tile');
+
 for (i = 0; i < tileArray.length; i++) {
     let idNo = tileArray[i].classList[2];
-    tileArray[i].addEventListener('click', () => { play(idNo) });
+    tileArray[i].addEventListener('click', () => {
+        play(idNo)
+    });
 }
-let a;
+
 function play(x) {
+
     let audio = new Audio(`Resources/Beats/0_${x}.flac`)
     audio.play();
-    setInterval(() => {
-        let playbackPercent = audio.currentTime * 100 / audio.duration;
-        let bgColor = getComputedStyle(document.getElementsByClassName(x)[0]).background;
-        document.getElementsByClassName(x)[0].style.background = `linear-gradient(to right, transparent ${playbackPercent - 20}%, #fff ${playbackPercent}%, transparent ${playbackPercent}%), ${bgColor}`;
-    } , 50)
+
+    document.getElementsByClassName(x)[0].classList.add('active');
+    const interval = setInterval(() => {
+        //console.log(audio.currentTime + ' of' + ' ' + audio.duration);
+        if (audio.currentTime === audio.duration) {
+            document.getElementsByClassName(x)[0].classList.remove('active');
+            audio.pause;
+            audio.currentTime = 0;
+            removeInterval();
+        }
+    }, 1000)
+
+    function removeInterval() {
+        clearInterval(interval);
+    }
 }
