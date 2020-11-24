8e0c64cd0077e1a980353b988dc36f17c52b0fb7
mmaismma <mmaismma@hotmail.com>
2020-11-24 19:17:29 +0530

    Changed a lot of things and we are now in v2.0

--- script.js
+++ script.js
@@ -1,67 +1,66 @@
-const tileArray = document.getElementsByClassName('tile');
+const tileArray = document.getElementsByClassName("tile");
 
-const beat01 = new Audio(`Resources/Beats/0_01.flac`);
-const beat02 = new Audio(`Resources/Beats/0_02.flac`);
-const beat04 = new Audio(`Resources/Beats/0_04.flac`);
-const beat06 = new Audio(`Resources/Beats/0_06.flac`);
-const beat07 = new Audio(`Resources/Beats/0_07.flac`);
-const beat08 = new Audio(`Resources/Beats/0_08.flac`);
-const beat09 = new Audio(`Resources/Beats/0_09.flac`);
-const beat11 = new Audio(`Resources/Beats/0_11.flac`);
-const beat12 = new Audio(`Resources/Beats/0_12.flac`);
-const beat16 = new Audio(`Resources/Beats/0_16.flac`);
-const beat18 = new Audio(`Resources/Beats/0_18.flac`);
-const beat21 = new Audio(`Resources/Beats/0_21.flac`);
-const beat22 = new Audio(`Resources/Beats/0_22.flac`);
-const beat24 = new Audio(`Resources/Beats/0_24.flac`);
-const beat25 = new Audio(`Resources/Beats/0_25.flac`);
-const beat28 = new Audio(`Resources/Beats/0_28.flac`);
-const beat29 = new Audio(`Resources/Beats/0_29.flac`);
-const beat31 = new Audio(`Resources/Beats/0_31.flac`);
-const beat32 = new Audio(`Resources/Beats/0_32.flac`);
-const beat33 = new Audio(`Resources/Beats/0_33.flac`);
-const beat35 = new Audio(`Resources/Beats/0_35.flac`);
-const beat41 = new Audio(`Resources/Beats/0_41.flac`);
-const beat44 = new Audio(`Resources/Beats/0_44.flac`);
-const beat47 = new Audio(`Resources/Beats/0_47.flac`);
+var beat01 = new Audio(`Resources/Beats/0_01.flac`),
+    beat02 = new Audio(`Resources/Beats/0_02.flac`),
+    beat04 = new Audio(`Resources/Beats/0_04.flac`),
+    beat06 = new Audio(`Resources/Beats/0_06.flac`),
+    beat07 = new Audio(`Resources/Beats/0_07.flac`),
+    beat08 = new Audio(`Resources/Beats/0_08.flac`),
+    beat09 = new Audio(`Resources/Beats/0_09.flac`),
+    beat11 = new Audio(`Resources/Beats/0_11.flac`),
+    beat12 = new Audio(`Resources/Beats/0_12.flac`),
+    beat16 = new Audio(`Resources/Beats/0_16.flac`),
+    beat18 = new Audio(`Resources/Beats/0_18.flac`),
+    beat21 = new Audio(`Resources/Beats/0_21.flac`),
+    beat22 = new Audio(`Resources/Beats/0_22.flac`),
+    beat24 = new Audio(`Resources/Beats/0_24.flac`),
+    beat25 = new Audio(`Resources/Beats/0_25.flac`),
+    beat28 = new Audio(`Resources/Beats/0_28.flac`),
+    beat29 = new Audio(`Resources/Beats/0_29.flac`),
+    beat31 = new Audio(`Resources/Beats/0_31.flac`),
+    beat32 = new Audio(`Resources/Beats/0_32.flac`),
+    beat33 = new Audio(`Resources/Beats/0_33.flac`),
+    beat35 = new Audio(`Resources/Beats/0_35.flac`),
+    beat41 = new Audio(`Resources/Beats/0_41.flac`),
+    beat44 = new Audio(`Resources/Beats/0_44.flac`),
+    beat47 = new Audio(`Resources/Beats/0_47.flac`);
 
-for (i = 0; i < tileArray.length; i++) {
-    let idNo = tileArray[i].classList[2];
-    tileArray[i].addEventListener('click', () => {
-        play(idNo)
+for (let i = 0; i < tileArray.length; i++) {
+    let beatNo = tileArray[i].dataset.beat;
+    tileArray[i].addEventListener("mousedown", () => {
+        play(beatNo, tileArray[i]);
     });
 }
 
-function play(x) {
-    let element = document.getElementsByClassName(x)[0];
-
-    let beat = eval("beat" + x);
-    // Try eliminating eval with Function
-    beat.play();
-
-    let interval;
-
-    function removeInterval() {
-        element.classList.remove('active');
-        beat.pause();
-        beat.currentTime = 0;
-        clearInterval(interval);
-    }
-
-    if (element.classList[3] === 'active') {
-        removeInterval();
-    } else {
-        element.classList.add('active');
-        interval = setInterval(() => {
-            //console.log(audio.currentTime + ' of' + ' ' + audio.duration);
-            if (beat.currentTime === beat.duration) {
-                removeInterval();
-            }
-        }, 1000)
-    }
+function removeInterval(elm, beat) {
+    beat.pause();
+    elm.classList.remove("active")
+    beat.currentTime = 0;
+    clearInterval(play[interval]);
 }
 
-document.getElementById('shareButton').addEventListener("click", async () => {
+function play(x, elm) {
+    let beat = window["beat" + x];
+    
+    if (!elm.classList.contains("active")) {
+        beat.play();
+        var interval = setInterval(() => {
+            if (beat.currentTime === beat.duration && elm.dataset.repeat !== "false") {
+                beat.currentTime = 0;
+                beat.play()
+            }
+            if (beat.currentTime === beat.duration && elm.dataset.repeat === "false") {
+                removeInterval(elm, beat);
+            }
+        }, 10)
+    } else {
+        removeInterval(elm, beat);
+    }
+
+    elm.classList.toggle("active")
+}
+
+document.getElementById("shareButton").addEventListener("click", async () => {
     try {
         await navigator.share({
             title: "Beatmaker by MMA",
@@ -74,17 +73,17 @@ document.getElementById('shareButton').addEventListener("click", async () => {
 });
 
 let installPrompt;
-window.addEventListener('beforeinstallprompt', (e) => {
+window.addEventListener("beforeinstallprompt", (e) => {
     e.preventDefault();
-    document.getElementById('installButton').style.display = 'unset';
+    document.getElementById("installButton").style.display = "unset";
     installPrompt = e;
 });
 
-document.getElementById('installButton').addEventListener('click', () => {
+document.getElementById("installButton").addEventListener("click", () => {
     installPrompt.prompt();
 })
 
-window.addEventListener('appinstalled', () => {
+window.addEventListener("appinstalled", () => {
     confetti.start();
     setTimeout(() => {
         confetti.stop();
